b5baec3e8c912568f3438ab39fa9f8d9fc44e5a4
Roger Iyengar <ri@rogeriyengar.com>
2019-09-05 15:00:23 -0400

    Added send error message

--- cognitive_engine.py
+++ cognitive_engine.py
@@ -1,6 +1,13 @@
 from abc import ABC
 from abc import abstractmethod
 
+
+
+def error_output(frame_id):
+    output = gabriel_pb2.Output()
+    output.status = gabriel_pb2.Output.Status.WRONG_INPUT_FORMAT
+
+
 class Engine(ABC):
     @abstractmethod
     def handle(self, input):
