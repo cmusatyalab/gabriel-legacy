780223f19c5ab9f515a34e8abf56c820a2b8716d
Roger Iyengar <ri@rogeriyengar.com>
2019-10-03 18:51:18 -0400

    pypi version 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gabriel-server",
-    version="0.0.3",
+    version="0.0.4",
     author="Roger Iyengar",
     author_email="ri@rogeriyengar.com",
     description="Server for Wearable Cognitive Assistance Applications",
