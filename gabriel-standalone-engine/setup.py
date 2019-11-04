682ddd88904a02528907c4621070e89b761ffba0
Roger Iyengar <ri@rogeriyengar.com>
2019-11-03 19:56:19 -0500

    separated server and standalone engine

--- gabriel-standalone-engine/setup.py
+++ gabriel-standalone-engine/setup.py
@@ -1,32 +1,36 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+DESCRIPTION = "Standalone Engine Runner for Gabriel Server. This must be used"
+              " with gabriel-server."
+
 
 setuptools.setup(
     name="gabriel-server",
-    version="0.0.9",
+    version="0.0.1",
     author="Roger Iyengar",
     author_email="ri@rogeriyengar.com",
-    description="Server for Wearable Cognitive Assistance Applications",
-    long_description=long_description,
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/cmusatyalab/gabriel-server-common",
+    url="http://gabriel.cs.cmu.edu",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
     license="Apache",
     classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.5",
     install_requires=[
-        "gabriel-protocol==0.0.3",
-        "websockets==8.0; python_version>=\"3.6\"",
-        "websockets==7.0; python_version<\"3.6\"",
         "pyzmq==18.1",
     ],
 )
